4c08d8b92b2c877e45bf052f7edbde22c314a41a
kylinz007 <vagrant@homestead>
2018-04-20 14:46:53 +0800

    layouts views

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function create() {
+        return view('users.create');
+    }
+}
